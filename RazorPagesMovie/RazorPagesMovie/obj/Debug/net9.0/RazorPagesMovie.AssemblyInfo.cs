235898d16653d5199138550edbd1b3a9052098a7
kerusha <keruz.gov@gmail.com>
2025-06-19 22:12:01 +0200

    Added Seed Data

--- RazorPagesMovie/RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e6c4b44e20d5cc7e8978ed0980812c08e75ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5cc181a9961f85674e3d93a642a82eba0ad3f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
