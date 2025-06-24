1946f37513743144cc1aec784b1947282e23103e
kerusha <keruz.gov@gmail.com>
2025-06-24 21:43:31 +0200

    Added data annotations to Movie and added migrations

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -7,16 +7,19 @@ public class Movie
 {
     public int Id { get; set; }
 
-    public string? Title { get; set; }
+    [Required, StringLength(60, MinimumLength = 3)]
+    public string Title { get; set; } = string.Empty;
 
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+    [DataType(DataType.Date), Display(Name = "Release Date")]
     public DateTime ReleaseDate { get; set; }
 
-    public string? Genre { get; set; }
+    [Required, StringLength(30), RegularExpression(@"^[A-Z]+[a-zA-z\s]*$")]
+    public string Genre { get; set; } = string.Empty;
 
+    [DataType(DataType.Currency), Range(1, 100)]
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 
+    [Required, StringLength(5), RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
     public string Rating { get; set; } = string.Empty;
 }
